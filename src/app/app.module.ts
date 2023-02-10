5117edd8c482e97cab5ad8bbab7d5af0775f9ba5
Van <lvanlowe@inrvisions.com>
2023-02-10 10:01:29 -0500

    feat: add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router'
 import { ReactFormsComponent } from './react-forms/react-forms.component';
 
+
+const routes: Routes = [
+  { path: 'home', component: ReactFormsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ReactFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
