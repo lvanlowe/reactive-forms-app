4bf74bc0e62e54c2a8b853e6291d55d7bd10ddde
Van <lvanlowe@inrvisions.com>
2023-02-10 09:44:40 -0500

    feat: add blank form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactFormsComponent } from './react-forms/react-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactFormsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/react-forms/react-forms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-react-forms',
+  templateUrl: './react-forms.component.html',
+  styleUrls: ['./react-forms.component.css']
+})
+export class ReactFormsComponent {
+
+}
